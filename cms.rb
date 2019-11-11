08defd61012e5829d1082b542d5bd3660878d4c9
John Isom <John.Isom2001@gmail.com>
2019-11-11 16:31:24 -0700

    Add access to files

--- cms.rb
+++ cms.rb
@@ -2,7 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+root = File.expand_path('..', __FILE__)
+
 get '/' do
   @filenames = Dir['./data/*'].map { |path| File.basename(path) }
   erb :index, layout: :layout
+end
+
+get '/:filename' do |filename|
+  headers['Content-Type'] = 'text/plain'
+  File.read("#{root}/data/#{filename}")
 end
